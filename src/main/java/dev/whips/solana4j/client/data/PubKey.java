06c0bba78981dd30cccfd5f8dfcd5239f1a51ac8
Chase Whipple <chasewhip20@gmail.com>
2022-06-23 17:38:02 -0600

    Correctly mention the pubkey in the constructors error

--- src/main/java/dev/whips/solana4j/client/data/PubKey.java
+++ src/main/java/dev/whips/solana4j/client/data/PubKey.java
@@ -3,6 +3,8 @@ package dev.whips.solana4j.client.data;
 import com.fasterxml.jackson.annotation.JsonValue;
 import io.github.novacrypto.base58.Base58;
 
+import java.util.Arrays;
+
 public class PubKey {
     public static final int KEY_LENGTH = 32;
 
@@ -11,7 +13,7 @@ public class PubKey {
 
     public PubKey(String base58PubKey){
         if (base58PubKey.length() < KEY_LENGTH){
-            throw new IllegalArgumentException("Public key is incorrect size.");
+            throw new IllegalArgumentException("Public key is incorrect size. " + base58PubKey);
         }
 
         this.stringKey = base58PubKey;
@@ -20,7 +22,7 @@ public class PubKey {
 
     public PubKey(byte[] rawKey){
         if (rawKey.length > KEY_LENGTH){
-            throw new IllegalArgumentException("Public key is incorrect size.");
+            throw new IllegalArgumentException("Raw Public key is incorrect size." + Arrays.toString(rawKey));
         }
 
         this.stringKey = Base58.base58Encode(rawKey);
